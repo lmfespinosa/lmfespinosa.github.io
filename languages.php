f8c0827d5b9870ffe9f43f6138582c8dadd1700e
Luis Miguel Fernández Espinosa <lmfespinosa@gmail.com>
2022-02-07 22:46:05 +0100

    Added languages.

---
+++ languages.php
@@ -0,0 +1,19 @@
+<?php
+
+function __($str, $lang = null){
+	
+	if ( $lang != null ){
+
+		if ( file_exists('languages/'.$lang.'.php') ){
+
+			include('languages/'.$lang.'.php');
+			if ( isset($texts[$str]) ){
+				$str = $texts[$str];
+			}
+		}
+	}
+
+	return $str;
+	
+}
+?>
